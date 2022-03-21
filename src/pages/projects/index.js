3042df25a858c02d7fcef66b07e2290ecd05905f
depechedave <depechedave@gmail.com>
2022-03-21 11:42:47 +0000

    disable hyperlink from project tiles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -12,7 +12,7 @@ const ProjectsHome = ({ data }) => {
           <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {projects.map(project => (
               <Link
-                to={"/projects/" + project.frontmatter.slug}
+                // to={"/projects/" + project.frontmatter.url}
                 key={project.id}
               >
                 <div>
@@ -20,7 +20,6 @@ const ProjectsHome = ({ data }) => {
                     <img
                       src={project.frontmatter.imgSrc}
                       alt={project.frontmatter.title}
-                      className=""
                     />
                   </div>
                   <div className="mt-2">
