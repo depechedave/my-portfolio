476a36c36750efd696b08053ac3a256652cd5402
depechedave <depechedave@gmail.com>
2022-04-06 14:21:05 +0100

    files clean up - to merge with main

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react"
+const Layout = require("./src/components/Layout").default
 
 export function wrapPageElement({ element, props }) {
-  const Layout = element.type.Layout ?? React.Fragment
   return <Layout {...props}>{element}</Layout>
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import React from "react"
 import { Link } from "gatsby"
 import { Disclosure } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
